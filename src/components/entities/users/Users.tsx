51c37aca04a0e360b3a94bc32af50a318328f2df
Andreizayaz <poznavauka@mail.ru>
2024-02-01 15:04:26 +0300

    [develop]: move sort functions to custom hook

--- src/components/entities/users/Users.tsx
+++ src/components/entities/users/Users.tsx
@@ -1,50 +1,14 @@
-import { FC, ReactElement, useEffect, useState } from "react";
-
-import { useDispatch, useSelector } from "react-redux";
+import { FC, ReactElement } from "react";
 
 import { Table } from "src/components/shared";
 
-import { selectUsers, setUsers, sortUsers } from "./store";
-
-import { getUsers } from "./helpers/functions";
-import { TOKENS, TOKENS_SORT_KEY, USERS_URL, headings } from "./helpers/consts";
+import { useUsers } from "./hooks/useUsers";
 
 import "./Users.scss";
 
 export const Users: FC = (): ReactElement => {
-  const users = useSelector(selectUsers);
-  const dispatch = useDispatch();
-  const [isDescSort, setIsAscSort] = useState(false);
+  const { currUsers, currHeadings } = useUsers();
 
-  const sortByTokens = () => {
-    setIsAscSort(!isDescSort);
-    dispatch(sortUsers({ isDescSort, sortKey: TOKENS_SORT_KEY }));
-  };
-
-  const currUsers = users.map(({ id, userData }) => {
-    return {
-      id,
-      userData: Object.values(userData).map((item) => {
-        return { content: item };
-      }),
-    };
-  });
-
-  const currHeadings = headings.map((item) => {
-    if (item.heading === TOKENS) {
-      return {
-        ...item,
-        classes: `${!isDescSort ? "sort" : "sort sort_desc"}`,
-        clickHandler: sortByTokens,
-      };
-    }
-
-    return { ...item };
-  });
-
-  useEffect(() => {
-    getUsers(USERS_URL).then((data) => dispatch(setUsers(data)));
-  }, []);
   return (
     <div className="users flex-column">
       <Table
